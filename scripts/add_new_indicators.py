20d5a7986d05b265b958e626580d4c0910eb1ed3
Brock Fanning <brockfanning@gmail.com>
2020-06-21 16:12:20 -0400

    Updated translations for new indicators

--- scripts/add_new_indicators.py
+++ scripts/add_new_indicators.py
@@ -5,19 +5,20 @@ import os
 def normalize_indicator_id(inid):
   return inid.strip('.').replace('.', '-')
 
+translation_files = [
+  os.path.join('translations', 'en', 'global_indicators.yml'),
+  os.path.join('translations', 'en', 'national_indicators.yml'),
+  os.path.join('translations', 'ru', 'global_indicators.yml'),
+  os.path.join('translations', 'ru', 'national_indicators.yml'),
+  os.path.join('translations', 'kk', 'global_indicators.yml'),
+  os.path.join('translations', 'kk', 'national_indicators.yml'),
+]
+
 source = pd.read_excel(
   'new_indicators.xlsx',
   usecols=[1,2,3,4,5,6,7],
   header=None,
-  names=[
-    'indicator',
-    'en-global',
-    'en-national',
-    'ru-global',
-    'ru-national',
-    'kk-global',
-    'kk-national',
-  ],
+  names=['indicator'] + translation_files,
   skiprows=[0,1,2]
 )
 for index,row in source.iterrows():
@@ -41,18 +42,30 @@ for index,row in source.iterrows():
       print('Warning: ' + meta_path + ' needs to be deleted.')
 
   else:
+    # Write the data and metadata.
     parts = indicator.split('-')
     data = 'Year,Units,Value'
     with open(data_path, 'w') as file:
       file.write(data)
+    translation_key = indicator + '-title'
     meta = {
       'indicator_number': indicator.replace('-', '.'),
       'goal_number': parts[0],
       'target_number': parts[0] + '.' + parts[1],
       'reporting_status': 'notstarted',
-      'indicator_name': 'global_indicators.' + indicator + '-title',
-      'indicator_available': 'national_indicators.' + indicator + '-title',
+      'indicator_name': 'global_indicators.' + translation_key,
+      'indicator_available': 'national_indicators.' + translation_key,
     }
     with open(meta_path, 'w') as file:
       yaml_str = yaml.dump(meta)
       file.write('---' + os.linesep + yaml_str + '---')
+
+    # Write the translations.
+    for translation_file in translation_files:
+      if not pd.isna(row[translation_file]):
+        translation_dict = {}
+        with open(translation_file, 'r') as file:
+          translation_dict = yaml.safe_load(file)
+        translation_dict[translation_key] = row[translation_file]
+        with open(translation_file, 'w') as file:
+          yaml.dump(translation_dict, file, sort_keys=True, encoding='utf-8', allow_unicode=True)
