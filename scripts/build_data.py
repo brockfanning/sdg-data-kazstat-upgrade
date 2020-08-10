4df4dc23de04ae9940519c9a69b90ac01c302883
Brock Fanning <brockfanning@gmail.com>
2020-08-10 02:38:29 -0400

    Alter metadata to determine global/national

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,11 @@
 from sdg.open_sdg import open_sdg_build
 
-open_sdg_build(config='config_data.yml')
+def alter_meta(metadata):
+    indicator_id = metadata['indicator_number']
+    id_parts = indicator_id.split('.')
+    is_global_indicator = len(id_parts) == 3
+    metadata['is_global_indicator'] = is_global_indicator
+    return metadata
+
+
+open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
